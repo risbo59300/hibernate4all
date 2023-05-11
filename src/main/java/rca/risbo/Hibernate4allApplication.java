cb0f6004a7efc96c2d16b74b4b4b5ed6876fa4fb
yb.gaba-mano <ybgabamano@gmail.com>
2023-05-11 23:56:08 +0200

    initialisation du projet

---
+++ src/main/java/rca/risbo/Hibernate4allApplication.java
@@ -0,0 +1,13 @@
+package rca.risbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate4allApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rca/risbo/Hibernate4allApplicationTests.java
@@ -0,0 +1,13 @@
+package rca.risbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate4allApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
